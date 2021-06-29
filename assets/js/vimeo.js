532f03f8cfd708858ccc04308718cd3150e5c81f
Aaron <mcdulltii@gmail.com>
2021-06-29 09:30:38 +0800

    Update vimeo.js

--- assets/js/vimeo.js
+++ assets/js/vimeo.js
@@ -11,17 +11,16 @@ function checkBackground() {
             vimeo.appendChild(iframe);
         }
         vimeo.children[0].src="https://player.vimeo.com/video/368071353?autopause=0&muted=1&color=000000&portrait=0&background=1&autoplay=1&loop=1&byline=0&title=0&controls=0";
-        document.body.background = "";
+        document.body.style.backgroundImage = "";
     } else {
         if (vimeo.children.length != 0) {
             vimeo.children[0].remove();
         }
-        document.body.background = "https://i.vimeocdn.com/video/824643252?mw=960&mh=540";
+        document.body.style.backgroundImage = 'url("https://i.vimeocdn.com/video/824643252?mw=960&mh=540")';
     }
 }
 
 $(document).ready(function() {
     checkBackground();
+    window.onresize = checkBackground;
 });
-
-window.onresize = checkBackground;
