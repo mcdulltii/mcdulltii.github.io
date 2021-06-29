0f3bbe579b297a5847b32a97137b0653cee5f1b4
Aaron <mcdulltii@gmail.com>
2021-06-29 09:15:15 +0800

    Update vimeo.js

--- assets/js/vimeo.js
+++ assets/js/vimeo.js
@@ -7,9 +7,20 @@ window.mobileCheck = function() {
 function checkBackground() {
     var vimeo = $('.vimeo-wrapper')[0];
     if (!mobileCheck()) {
+        if (vimeo.children.length == 0) {
+            const iframe = document.createElement("iframe");
+            iframe.setAttribute("webkitallowfullscreen","");
+            iframe.setAttribute("mozallowfullscreen","");
+            iframe.setAttribute("allowfullscreen","");
+            iframe.setAttribute("src","");
+            iframe.setAttribute("frameborder","0");
+            vimeo.appendChild(iframe);
+        }
         vimeo.children[0].src="https://player.vimeo.com/video/368071353?autopause=0&muted=1&color=000000&portrait=0&background=1&autoplay=1&loop=1&byline=0&title=0&controls=0";
     } else {
-        vimeo.remove();
+        if (vimeo.children.length != 0) {
+            vimeo.children[0].remove();
+        }
         document.body.background = "https://i.vimeocdn.com/video/824643252?mw=960&mh=540";
     }
 }
@@ -18,4 +29,4 @@ $(document).ready(function() {
     checkBackground();
 });
 
-window.addEventListener('resize', checkBackground);
+window.onresize = checkBackground;
